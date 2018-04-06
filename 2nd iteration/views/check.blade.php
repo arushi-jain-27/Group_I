c29818aa6fc6957d2055432ffbe6e31bfec34ecc
Pushpendra Kumar <Pk46113@gmail.com>
2018-04-07 01:49:01 +0530

    made to verify entry by admin

---
+++ 2nd iteration/views/check.blade.php
@@ -0,0 +1,141 @@
+
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>List of Posts</title>
+  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  
+      <style>
+      /* NOTE: The styles were added inline because Prefixfree needs access to your styles and they must be inlined if they are on local disk! */
+      body {
+  font-family: "Source Sans Pro", "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
+}
+
+a {
+  color: #000;
+  text-decoration: none;
+}
+
+.wrapper {
+  width: 100%;
+  max-width: 800px;
+  margin: 2rem auto;
+  position: relative;
+}
+
+.post-list-subhead {
+  text-transform: uppercase;
+  color: #aaa;
+  border-bottom: 1px solid #eee;
+  padding: 0.4em 0;
+  font-size: 80%;
+  font-weight: 500;
+  letter-spacing: 0.1em;
+}
+
+.post-item {
+  padding: 10px 20px;
+  margin: 20px 0;
+  border-left: 1px solid #EEE;
+}
+.post-item:hover {
+  background-color: #ddd;
+}
+
+.post-item-title {
+  font-size: 1.25em;
+  color: #222;
+  font-weight: bold;
+}
+.post{
+  font-size: 125%;
+  padding :0.5em;
+  color: #6495ED;
+  margin-top: 1em;
+}
+.post-item-category {
+  margin: 0 0.1em;
+  padding: 0.2em 1em;
+  color: #fff;
+  background: #000;
+  font-size: 80%;
+  text-decoration: none;
+}
+
+.post-item-more {
+  text-transform: uppercase;
+  border: 0.1px solid #000;
+  padding: 2px 8px;
+  margin: 0;
+}
+.abc{
+  font-size: 175%;
+  color: #6495ED;
+  padding: 2px 8px;
+  
+  margin-top: 1em;
+}
+
+    </style>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+
+</head>
+
+<body>
+
+  <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,900,400italic|Crimson+Text:400,600' type='text/css' rel='stylesheet' />
+  <center class='abc'>The Result is :</center>
+<?php  
+ 
+echo "<a>      
+<div class='wrapper'>
+<div class='post-list'>  
+<section class='post-item'>
+        <header >
+            <h2 class='post-item-title'>Title : </h2>". $results[0]->t."
+        </header>
+        <header >
+            <h2 class='post-item-title'>Author : </h2>". $results[0]->a."
+        </header>
+        <header >
+            <h2 class='post-item-title'>Genre : </h2>". $results[0]->g."
+        </header>
+        <header >
+            <h2 class='post-item-title'>Language : </h2>". $results[0]->l."
+        </header>
+        <header >
+            <h2 class='post-item-title'>Publisher : </h2>". $results[0]->p."
+        </header>
+        <header >
+            <h2 class='post-item-title'>Keywords : </h2>". $results[0]->k."
+        </header>
+        <header >
+            <h2 class='post-item-title'>Description : </h2>". $results[0]->d."
+        </header>
+    </section>
+</div>
+</div></a>";
+?>
+<form method="post" action="/add" enctype="multipart/form-data" >
+  {{ csrf_field() }}
+  <div class="form-group">
+  <select name='m' >
+  <option value="i">Insert</option>
+  <option value="e">Remove</option>
+  </select>
+  </div>
+  <div class="form-group">
+   <input type="hidden" name="ide" value=<?php echo  $results[0]->n; ?> readonly>
+   </div>
+   <button type="submit" class="btn btn-primary">Submit</button>
+   </form>
+</body>
+
+</body>
+
+</html>
